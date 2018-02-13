41febe9be9bc9495dab4209b898908418a77d9b5
JFulford85 <JMFulford85@gmail.com>
2018-02-13 11:10:23 -0600

    Add project files.

---
+++ CarnivalAdventure2018/CarnivalConsoleUI.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CarnivalAdventure2018
+{
+    class CarnivalConsoleUI
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
